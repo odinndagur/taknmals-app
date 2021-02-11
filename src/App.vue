d13eeffd1be8edf8c46baab8ded5ee86d0989707
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2021-02-11 08:01:25 +0000

    firstimport

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="searchBox">
+<input v-model="search"/>
+</div>
+<br/>
+
+    <SignCard class="sign-card2" v-for="sign in currentArr" :key="sign.youtubeId" :sign="sign" @select="select(sign)" :selected="isSelected(sign)" :class="{selected: isSelected(sign)}"/> 
+<br/>
+<button @click="loadMore">Add more signs</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import taknjson from "./assets/takn.json";
+import SignCard from './components/SignCard'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components:{
+    SignCard,
+  },
+  data: () => {
+    return {
+      takn: taknjson,
+      currentArr: [],
+      selected: {},
+      search:'',
+    };
+  },
+  methods: {
+    getInitial(){
+      for(let i = 0; i < 40; i++){
+        this.currentArr.push(this.takn[i]);
+      }
+    },
+    loadMore(){
+      let initial = this.currentArr.length;
+      for(let i = initial; i < initial + 10; i++){
+        this.currentArr.push(this.takn[i]);
+      }
+    },
+    select(sign){
+      console.log(sign.phrase)
+      if(this.selected == sign){
+        this.selected = {};
+      }
+      else{
+        this.selected = sign;
+      }
+    },
+    isSelected(sign){
+      return this.selected == sign;
+    },
+    scroll () {
+  window.onscroll = () => {
+    let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+
+    if (bottomOfWindow) {
+      this.loadMore();
+        }
+ }
 }
+  },
+  computed: {
+filteredList(){
+  return this.takn.filter(takn => {
+    return takn.phrase.toLowerCase().includes(this.search.toLowerCase());
+  })
+},
+  },
+  mounted(){
+    this.getInitial();
+    this.scroll();
+  }
+};
 </script>
 
 <style>
@@ -24,5 +83,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.sign-card {
+  display:inline-block;
+  /* margin:auto; */
+  width:30px;
+}
+.selected{
+/* margin:20px; */
 }
 </style>
---
+++ src/components/SignCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="sign-card">
+    <div class="centered" v-if="!selected" :class="{ isSelected: selected }">
+    <a @click="$emit('select')" :class="{ isSelected: selected }">{{sign.phrase}}</a>
+    </div>
+
+
+    <div class="centered" v-if="selected" :class="{ isSelected: selected }">
+    <a @click="$emit('select')" :class="{ isSelected: selected }">{{sign.phrase}}</a> <br/>
+
+        <div class="player">
+      <youtube
+        v-if="selected"
+        :video-id="sign.youtubeId"
+        :fitParent="true"
+      ></youtube>
+    </div>
+
+<div class="list">
+<ul>
+  <li v-for="word in sign.related" :key="word">
+{{word}}
+  </li>
+</ul>
+</div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignCard",
+  props: {
+    sign: {},
+    selected: Boolean,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list {
+  margin:auto;
+  vertical-align: middle;
+}
+.sign-card {
+  width:80%;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 10px 10px;
+}
+a {
+  color: #gray;
+}
+a:hover{
+  cursor: pointer;
+}
+.isSelected {
+  color: red;
+}
+
+/* @media screen and (max-width: 600px) {
+  .player {
+    width: 400px;
+    margin: auto;
+  }
+} */
+
+.player {
+  width:80%;
+  max-width: 450px;
+  float:left;
+  margin: auto;
+}
+</style>
